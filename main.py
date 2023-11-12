cc95824cc863b7debe3bbf41d319d778ca01688e
Cody.Diao <137848362+CodyDiao@users.noreply.github.com>
2023-11-11 17:36:01 -0700

    changes

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 import Functions as fn
-=======
-import Functions as FN
-
-user = FN.user
-
-total = user.total_emissions()
-print(f"Your total emissions for a year is: {total} Tons CO2")
->>>>>>> 79f464c (stats works now)
 
 
 def total_emissions():
@@ -16,7 +7,6 @@ def total_emissions():
     )  # base it off of calories needed for average person that age
     flights_per_year = int(input("Enter number of flights per year: "))
     daily_drive_km = float(input("Enter average daily kilonmeters driven: "))
-<<<<<<< HEAD
     car_type = fn.get_car_type()
 
     travel_km_values = { "Petrol Car":0.17, "Hybrid Car":0.068, "Electric Car": 0.047}
@@ -27,18 +17,6 @@ def total_emissions():
     # Add flight c02 calculations here
 
     total = co2_car + co2_flight + co2_age #converting to tons
-=======
-    car_type = get_car_type()
-
-    travel_km_values = {"Petrol Car": 0.17, "Hybrid Car": 0.068, "Electric Car": 0.047}
-    co2_car = travel_km_values[car_type] * daily_drive_km * 365  # the co2 is in kg
-
-    co2_flight = flights_per_year * 100000000
-    co2_age = 100000000
-    # Add flight c02 calculations here
-
-    total = (co2_car + co2_flight + co2_age) / 1000  # converting to tons
->>>>>>> 79f464c (stats works now)
     return total
 
 total = total_emissions()
