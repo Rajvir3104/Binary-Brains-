ca754235ac9c6a8deeb2149efc07d168d11974a4
gurlin.singh <gurlin.singh@ucalgary.ca>
2023-11-11 18:14:30 -0700

    made changes to fucntion file

--- Functions.py
+++ Functions.py
@@ -7,93 +7,102 @@ A Module Defining all of the Functions used for the Hack the Change Reso
 
 """
 
+class user:
+
+    def __init__(self, num_flights):
+        """
+            Initialize the angle and initial velocity of the projectile
+            
+            Parameters
+            ----------
+            
+            num_flights: intiger
+
+                The Number of flights that a user takes on an annual basis
+                
+            """
+        
+        self.num_flights = num_flights
+
+    def flight_emissions(number_of_flight):
+        """
+            Calculating the CO2 emissions per # of flight
+
+            Parameters
+            ----------
+            
+            Number of flights :int
+            
+                The Number of flights a user has in a years time (Tones of CO2)
+            
+        """
+        
+        flight = pd.read_csv(r"C:\Users\ashle\Documents\ENGG year 2\Binary-Brains-\Data\AIRTRANS_CO2_11112023191907048.csv")
+        flight_can = flight[flight["LOCATION"] == "CAN"]
+        flight_can = flight_can[flight_can["FREQUENCY"] == "A"]
+        flight_can_passenger = flight_can[flight_can["Flight type"] == "Passenger flights"]
 
 
-def flight_emissions(num_flights):
-    """
-        Calculating the CO2 emissions per # of flight
+        flight_can_passenger = flight_can_passenger.groupby(['Time'])["Value"].mean().reset_index()
 
-        Parameters
-        ----------
-        
-        Number of flights :int
-        
-            The Number of flights a user has in a years time (Tones of CO2)
-        
-    """
+        passengers_by_year = [133426703,140892544,150808451,160641587,162864077,46349535]
+
+        y =0
+        emission_per_flight = []
+        while y < 6:
+            x = flight_can_passenger.iloc[y]
+            x = x["Value"] / passengers_by_year[y]
+
+            emission_per_flight.append(x)
+
+            y+=1
+
+
+        emission_per_flight = statistics.mean(emission_per_flight) * number_of_flight
+
+        return(emission_per_flight)
     
-    flight = pd.read_csv(r"Data\AIRTRANS_CO2_11112023191907048.csv", low_memory =False)
-    flight_can = flight[flight["LOCATION"] == "CAN"]
-    flight_can = flight_can[flight_can["FREQUENCY"] == "A"]
-    flight_can_passenger = flight_can[flight_can["Flight type"] == "Passenger flights"]
+    def get_car_type():
+        """
+        This fucntion gets the input value for the type of car the user uses
+        """
+        # Define car types
+        car_types = {
+            1: 'Petrol Car',
+            2: 'Hybrid Car',
+            3: 'Electric Car',
+        }
 
+        # Display menu
+        print("Choose the type of car you own:")
+        for key, value in car_types.items():
+            print(f"{key}: {value}")
 
-    flight_can_passenger = flight_can_passenger.groupby(['Time'])["Value"].mean().reset_index()
+        user_choice = int(input("Enter the code for your car type: "))
 
-    passengers_by_year = [133426703,140892544,150808451,160641587,162864077,46349535]
+        if user_choice in car_types:
+            car_type = car_types[user_choice]
+            print(f"You selected: {car_type}")
+        else:
+            print("Invalid choice. Please select a valid car type.")
+        return car_type
 
-    y =0
-    emission_per_flight = []
-    while y < 6:
-        x = flight_can_passenger.iloc[y]
-        x = x["Value"] / passengers_by_year[y]
+    def total_emissions():
+        # age = int(input("Enter your age: ")) #base it off of calories needed for average person that age 
+        flights_per_year = int(input("Enter number of flights per year: "))
+        daily_drive_km = float(input("Enter average daily kilonmeters driven: "))
+        car_type = get_car_type() #fix this 
 
-        emission_per_flight.append(x)
+        travel_km_values = { "Petrol Car":0.17, "Hybrid Car":0.068, "Electric Car": 0.047}
+        co2_car = travel_km_values[car_type] * daily_drive_km * 365 #the co2 is in kg
 
-        y+=1
+        co2_flight = flights_per_year * 100000000
+        co2_age = 100000000
+        # Add flight c02 calculations here
 
+        total = (co2_car + co2_flight + co2_age)/1000 #converting to tons
+        return total
 
-    emission_per_flight = statistics.mean(emission_per_flight) * num_flights
-
-    return(emission_per_flight)
-
-def get_car_type():
-    """
-    This fucntion gets the input value for the type of car the user uses
-    """
-    # Define car types
-    car_types = {
-        1: 'Petrol Car',
-        2: 'Hybrid Car',
-        3: 'Electric Car',
-    }
-
-    # Display menu
-    print("Choose the type of car you own:")
-    for key, value in car_types.items():
-        print(f"{key}: {value}")
-
-    user_choice = int(input("Enter the code for your car type: "))
-
-    if user_choice in car_types:
-        car_type = car_types[user_choice]
-        print(f"You selected: {car_type}")
-    else:
-        print("Invalid choice. Please select a valid car type.")
-    return car_type
-
-def age_emissions(age):
-    """
-    The following data is the range of groups and average CO2 emissions
-    per person, excluding transportation and travel. So basic needs and shelter 
-    for the average person
-
-    The keys are the upper limit of the age groups
-    The values are tons of CO2 per year
-    """
-    ranges = {18: 3, 30: 7, 50: 10, 70: 9, 160: 7}
-
-    for key, val in ranges.items():
-        if age <= key:
-            return val
-
-    # If the age is greater than or equal to the largest key, return the value associated with it
-    return ranges[max(ranges)]
-
-
-
-
-    
 
 
 
